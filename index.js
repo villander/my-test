b0cf5a0425fc5555bc080f20968e59c81adb3eeb
Michael Villander <michaelvillander@Michaels-MacBook-Air.local>
2017-12-31 14:42:53 -0200

    Try add npm modules

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'my-test'
+  name: 'my-test',
+  isDevelopingAddon: function() {
+    return true;
+  },
+  included() {
+    this._super.included.apply(this, arguments);
+
+    this.import('vendor/apollo-client/index.js');
+  },
+
+  treeForVendor(vendorTree) {
+
+    var apolloCacheTree = new Funnel(path.join(this.project.root, 'node_modules', 'apollo-client'), {
+      files: ['index.js'],
+      destDir: 'apollo-client'
+    });
+
+    return new MergeTrees([vendorTree,  apolloCacheTree]);
+  },
 };
